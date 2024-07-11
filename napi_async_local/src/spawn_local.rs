c2de841e728778f05392eca249c1f3bcf83738de
David Alsh <alshdavid@gmail.com>
2024-07-12 07:53:47 +1000

    fmt

--- napi_async_local/src/spawn_local.rs
+++ napi_async_local/src/spawn_local.rs
@@ -14,27 +14,27 @@ use crate::store;
 use crate::JsRc;
 
 pub trait SpawnLocalExt {
-  /// Spawns a non-blocking future on the local thread. Returns a Promise with the value 
-  /// returned in the async closure. Normal [`NapiValue`] types can be interacted with in 
+  /// Spawns a non-blocking future on the local thread. Returns a Promise with the value
+  /// returned in the async closure. Normal [`NapiValue`] types can be interacted with in
   /// the async context. Supports channels, timers, etc.
-  /// 
+  ///
   /// To ensure the availability of [`NapiValue`] types beyond the life of the parent function scope,
   /// ensure that [`NapiValue`] types that will be used in an async closure are wrapped in a [`JsRc`].
-  /// 
+  ///
   /// ### Usage:
-  /// 
+  ///
   /// #### Running a Callback:
-  /// 
+  ///
   /// ```
   /// use std::time::Duration;
-  /// 
+  ///
   /// use napi::*;
   /// use napi_derive::napi;
   /// use napi_async_local::JsRc;
   /// use napi_async_local::JsRcExt;
   /// use napi_async_local::SpawnLocalExt;
   /// use async_std::task;
-  /// 
+  ///
   /// #[napi]
   /// fn my_js_func(env: Env, callback: JsRc<JsFunction>) -> napi::Result<JsObject> {
   ///   env.spawn_local(move |env| async move {
@@ -44,37 +44,37 @@ pub trait SpawnLocalExt {
   ///   })
   /// }
   /// ```
-  /// 
+  ///
   /// #### Using Channels:
-  /// 
+  ///
   /// ```
   /// use std::thread;
   /// use std::time::Duration;
-  /// 
+  ///
   /// use napi::*;
   /// use napi_derive::napi;
   /// use napi_async_local::JsRc;
   /// use napi_async_local::JsRcExt;
   /// use napi_async_local::SpawnLocalExt;
   /// use async_std::channel;
-  /// 
+  ///
   /// #[napi]
   /// fn my_js_func(env: Env, callback: JsRc<JsFunction>) -> napi::Result<JsObject> {
   ///   let (tx, rx) = channel::unbounded();
-  /// 
+  ///
   ///   thread::spawn(move || {
   ///     for i in 0..10 {
   ///       tx.send_blocking(i).unwrap();
   ///       thread::sleep(Duration::from_millis(1000));
   ///     }
   ///   });
-  /// 
+  ///
   ///   env.spawn_local(move |env| async move {
   ///     while let Ok(value) = rx.recv().await {
   ///       println!("Got number: {}", value);
   ///       callback.inner(&env)?.call(None, &[env.create_int32(value)?])?;
   ///     }
-  /// 
+  ///
   ///     Ok(())
   ///   })
   /// }
@@ -146,7 +146,7 @@ impl SpawnLocalExt for Env {
           }
           Err(error) => emit_error(error),
         }
-        
+
         store::delete_store_value(resolve_func_key);
         store::delete_store_value(reject_func_key);
       })?;
