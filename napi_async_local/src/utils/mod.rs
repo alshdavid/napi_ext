1f1f914e53a2c57d0e003764abe149acbbacc3be
David Alsh <alshdavid@gmail.com>
2024-07-31 11:29:31 +1000

    example

--- napi_async_local/src/utils/mod.rs
+++ napi_async_local/src/utils/mod.rs
@@ -1,11 +1,9 @@
 mod console_log;
 mod create_promise;
-// mod js_fn;
 mod js_promise;
 mod utils_ext;
 
 pub use self::console_log::*;
 pub use self::create_promise::*;
-// pub use self::js_fn::*;
 pub use self::js_promise::*;
 pub use self::utils_ext::*;
